48b86ec0b980340b285d688af66512c810599d0c
Aaron Janes <Aaron.Janes@gmail.com>
2021-01-13 17:53:20 -0700

    contsants folder

---
+++ constants.js
@@ -0,0 +1,23 @@
+const IP = '135.23.222.131';
+const PORT = 50542;
+const PLAYER_INITIALS = 'AJ';
+const MOVEUP = 'w';
+const MOVEDOWN = "s";
+const MOVERIGHT = "d";
+const MOVELEFT = "a";
+const MESS = {
+  m: "Yikes",
+  t: "Gooooo",
+  y: "lol"
+}
+
+module.exports = {
+  IP,
+  PORT,
+  PLAYER_INITIALS,
+  MOVEUP,
+  MOVEDOWN,
+  MOVERIGHT,
+  MOVELEFT,
+  MESS
+}
